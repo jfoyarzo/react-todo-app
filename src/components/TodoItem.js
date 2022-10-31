578f3c4b785e0f7b7b4337e5c1f58fe4c7a34c9f
jfoyarzo <foyarzo621@gmail.com>
2022-10-31 18:49:05 -0300

    Add linethrough style to completed items in the TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,16 +5,25 @@ import styles from './TodoItem.module.css';
 class TodoItem extends React.Component {
   render() {
     const { todo, handleChangeProps, deleteTodoProps } = this.props;
+    const { completed, id, title } = todo;
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
     return (
       <li className={styles.item}>
         <input
           type="checkbox"
           className={styles.checkbox}
-          checked={todo.completed}
-          onChange={() => handleChangeProps(todo.id)}
+          checked={completed}
+          onChange={() => handleChangeProps(id)}
         />
-        <button type="button" onClick={() => deleteTodoProps(todo.id)}>Delete</button>
-        {todo.title}
+        <button type="button" onClick={() => deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
       </li>
     );
   }
