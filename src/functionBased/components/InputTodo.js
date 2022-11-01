64423f17feb8e0a53fa3eea63298afa6036c6486
jfoyarzo <foyarzo621@gmail.com>
2022-11-01 14:53:19 -0300

    Update TodoContainer and InputTodo components to function-based

---
+++ src/functionBased/components/InputTodo.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const InputTodo = (props) => {
+  const [title, setTitle] = useState('');
+  const { addTodoProps } = props;
+
+  const onChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoProps(title);
+      setTitle('');
+    } else {
+      alert('Please write item');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={title}
+        name="title"
+        onChange={onChange}
+      />
+      <button type="submit" className="input-submit">Submit</button>
+    </form>
+  );
+};
+
+export default InputTodo;
