310698555bbe7eb8854fd9e29e6e819f43891fee
jfoyarzo <foyarzo621@gmail.com>
2022-11-01 21:31:16 -0300

    Add different paths for the SPA wit React Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TodoContainer from './functionBased/components/TodoContainer';
+import Navbar from './functionBased/components/Navbar';
+import About from './functionBased/pages/About';
+import SinglePage from './functionBased/pages/SinglePage';
+import NotMatch from './functionBased/pages/NotMatch';
 import './functionBased/App.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />}>
+          <Route path=":slug" element={<SinglePage />} />
+        </Route>
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
